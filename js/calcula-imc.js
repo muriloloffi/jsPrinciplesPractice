5bf5c7b1bb7aaefb3e6c444b411ea7b9db337366
Murilo <muriloloffi@gmail.com>
2021-07-05 22:34:15 -0300

    validacao de nome e altura

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -6,35 +6,46 @@ var pacientes = document.querySelectorAll(".paciente");
 for (var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
     var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
-
-    var tdImc = paciente.querySelector(".info-imc");
-
     var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
     var altura = tdAltura.textContent;
 
-    var alturaEhValida = true;
-    var pesoEhValido = true;
+    var tdImc = paciente.querySelector(".info-imc");
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhValido) {
         console.log("Peso inválido");
         tdImc.textContent = "Peso inválido!";
-        pesoEhValido = false;
-
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.0) {
+    if (!alturaEhValida) {
         console.log("Altura inválida");
         tdImc.textContent = "Altura inválida!";
-        alturaEhValida = false;
-
         paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 240) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura >= 0 && altura <= 3.0) {
+        return true;
+    } else {
+        return false;
     }
 }
 
--- js/form.js
+++ js/form.js
@@ -9,10 +9,18 @@ botaoAdicionar.addEventListener("click", function (event) {
 
     var pacienteTr = montaTr(paciente);
 
+    var erros = validaPaciente(paciente);
+
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+        return
+    }
+
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
+    limpaMsgsDeErro();
     form.reset();
 });
 
@@ -47,4 +55,35 @@ function montaTd(dado, classe) {
     td.classList.add(classe);
 
     return td;
+};
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagem-erro");
+    limpaMsgsDeErro();
+    erros.forEach(function (erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+};
+
+function limpaMsgsDeErro() {
+    var ul = document.querySelector("#mensagem-erro");
+    if (ul) {
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+    }
+};
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if (!validaPeso(paciente.peso))
+        erros.push("O peso é inválido");
+    if (!validaAltura(paciente.altura))
+        erros.push("A altura é inválida");
+
+    return erros;
 };
